2a1a945e0db49a52487cb026fdda6308ed37aff7
Ernestas <ernestas.luza@gmail.com>
2019-11-28 21:13:47 +0200

    no message

--- scripts/gamemanager.js
+++ scripts/gamemanager.js
@@ -7,13 +7,13 @@ let GameManager = {
   showHero: function(classType){
     switch (classType) {
       case "Warrior":
-        player = new Player(classType, 200, 50);
+        player = new Player(classType, 200, 15);
         break;
     case "Archer":
-      player = new Player(classType, 100, 100);
+      player = new Player(classType, 100, 20);
       break;
     case "Druid":
-      player = new Player(classType, 150, 99);
+      player = new Player(classType, 150, 8);
       break;
 
     }
@@ -54,11 +54,31 @@ let GameManager = {
   }
 
   getHeader.innerHTML = '<p>Choose your move</p>';
-  getAction.innerHTML = '<a href="#" class="btn-prefight">Attack</a>';
+  getAction.innerHTML = '<a href="#" class="btn-prefight" onclick="GameManager.Attack()">Attack</a>';
   getEnemy.innerHTML = '<img src="img/' + enemy.enemyType.toLowerCase() + '.jpg" ><div><h3>' + enemy.enemyType + '</h3><p class="health-enemy">Health:' + enemy.health + '</p><p>Damage:' + enemy.damage + '</p></div> ';
 
-
-
-
+    },
+  Attack: function(){
+    player.health -= enemy.damage;
+    enemy.health -= player.damage;
+    console.log(player.health);
+    //WHen monster attacks
+    let getHealth = document.querySelector(".health-player");
+    getHealth.innerHTML = '<p class="health-player">Health: ' + player.health +'</p>';
+    if (player.health  <= 0) {
+      getHealth.innerHTML ='<p class="health-player">Health: 0</p>';
+      alert("You are dead Hero! Go home...");
     }
+
+    //When Hero attacks
+    let getEnemyHealth =document.querySelector(".health-enemy");
+    getEnemyHealth.innerHTML = '<p class="health-enemy">Health:' + enemy.health + '</p>';
+    if (enemy.health <= 0)
+    {
+      getEnemyHealth.innerHTML = '<p class="health-enemy">Health: 0</p>';
+      alert("You won hero! You killed poor creature you wanker...");
+    }
+  }
+
+
   }
